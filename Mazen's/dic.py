a006955fef205dbf359693d7cc61ae3d1914f30e
khaledkhedr18 <khaledkhedr18@gmail.com>
2025-05-29 22:22:14 +0300

    Initial Commit
    Brought the source code for the two apps previously created to merge their code according to the current design

---
+++ Mazen's/dic.py
@@ -0,0 +1,83 @@
+vosk_languages = {
+    "English": "en",
+    "Arabic": "ar",
+    "Spanish": "es",
+    "French": "fr",
+    "German": "de",
+    "Russian": "ru",
+    "Portuguese": "pt",
+    "Chinese ": "cn",
+    "Turkish": "tr",
+    "Italian": "it",
+    "Ukrainian": "uk",
+    "Dutch": "nl",
+    "Hindi": "hi",
+    "Vietnamese": "vi",
+    "Korean": "ko",
+    "Japanese": "ja",
+    "Polish": "pl"
+}
+tesseract_languages = {
+    "English": "eng",
+    "Arabic": "ara",
+    "Spanish": "spa",
+    "French": "fra",
+    "German": "deu",
+    "Russian": "rus",
+    "Portuguese": "por",
+    "Chinese": "chi_sim",
+    "Turkish": "tur",
+    "Italian": "ita",
+    "Ukrainian": "ukr",
+    "Dutch": "nld",
+    "Hindi": "hin",
+    "Vietnamese": "vie",
+    "Korean": "kor",
+    "Japanese": "jpn",
+    "Polish": "pol"
+}
+
+
+vosk_to_tesseract = {
+    "en": "eng",
+    "ar": "ara",
+    "es": "spa",
+    "fr": "fra",
+    "de": "deu",
+    "ru": "rus",
+    "pt": "por",
+    "cn": "chi_sim",
+    "tr": "tur",
+    "it": "ita",
+    "uk": "ukr",
+    "nl": "nld",
+    "hi": "hin",
+    "vi": "vie",
+    "ko": "kor",
+    "ja": "jpn",
+    "pl": "pol"
+}
+
+vosk_model_paths = {
+    "en": r"/home/pi/Desktop/gradproj/vosk-model-small-en-us-0.15",
+    "ar": r"C:\Users\COMPUMARTS\Desktop\gradproj\vosk-model-ar-mgb2-0.4",
+    "es": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-es-0.42",
+    "fr": r"C:\Users\COMPUMARTS\Desktop\gradproj\vosk-model-small-fr-0.22",
+    "de": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-de-0.15",
+    "ru": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-ru-0.22",
+    "pt": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-pt-0.3",
+    "cn": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-cn-0.22",
+    "tr": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-tr-0.3",
+    "it": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-it-0.22",
+    "uk": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-uk-v3-nano",
+    "nl": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-nl-0.22",
+    "hi": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-hi-0.22",
+    "vi": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-vn-0.4",
+    "ko": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-ko-0.22",
+    "ja": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-ja-0.22",
+    "pl": r"C:\Users\COMPUMARTS\Desktop\vosk\gradproj\vosk-model-small-pl-0.22"
+
+
+}
+
+
